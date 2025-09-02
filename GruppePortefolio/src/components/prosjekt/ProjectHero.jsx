d673f327bdf963e0a1bfbb9a5e43bee57c3ff0aa
sarastray <sara.stray@online.no>
2025-09-02 14:19:11 +0200

    Fikset prosjektside

---
+++ GruppePortefolio/src/components/prosjekt/ProjectHero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Projects.css";
+
+function ProjectHero() {
+  return (
+    <section id="hero" className="hero-section">
+      <h2>Prosjekter</h2>
+    </section>
+  );
+}
+
+export default ProjectHero;
