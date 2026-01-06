338f0d0e6b854d57236a714c67bf5a481c4b8ab4
sarastray <sara.stray@online.no>
2026-01-06 12:23:54 +0100

    fikset liten feil

--- GruppePortefolio/src/components/bachelor/Bachelor.jsx
+++ GruppePortefolio/src/components/bachelor/Bachelor.jsx
@@ -4,11 +4,11 @@ import "./Bachelor.css";
 function Bachelor() {
   return (
     <section id="contact" className="contact-section">
-      <h2>Nordkart</h2>
-      <p>Prosjektbeskrivelse:</p>
+      <h2>Norkart</h2>
+      <p>Prosjektbeskrivelse: Utforske ulike måter å bruke KI i en planprossess, med fokus på formidling, forståelse, og kontroll av konsekvenseutredning</p>
 
     </section>
   );
 }
 
-export default Contact;
+export default Bachelor;
